ee39456a8538b2b7377d3672788dd8000fc39c47
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2019-09-29 09:16:43 +0300

    calculate_frequencies finished

--- lab_1/main.py
+++ lab_1/main.py
@@ -8,7 +8,22 @@ def calculate_frequences(text: str) -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    return {'dieses Wort': 56}
+    if isinstance(text, str) == False or text == '' or (text.isalpha() == False and text.count(' ') == 0):
+        return {}
+    text = text.lower()
+    for c in text:
+        if c.isalpha() == False:
+            text = text.replace(c, ' ')
+    while '  ' in text:
+        text = text.replace('  ', ' ')
+    list_of_words = text.split()
+    dict_of_freqs = {}
+    for elem in list_of_words:
+        if elem in dict_of_freqs:
+            dict_of_freqs[elem] += 1
+        else:
+            dict_of_freqs[elem] = 1
+    return dict_of_freqs
 
 def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
     """
